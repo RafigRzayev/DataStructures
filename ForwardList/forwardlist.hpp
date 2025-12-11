8e22a5c5daa5f4e5012848bdd562fe7c32335cdd
exoplanet1-admin <rafigrzayev7@gmail.com>
2025-12-11 16:33:19 +0400

    update comments

--- ForwardList/forwardlist.hpp
+++ ForwardList/forwardlist.hpp
@@ -8,12 +8,7 @@
  * - Implemented via sentinel node at the head. It simplifies eraseAfter() and insertAfter() via beforeBegin() iterator
  * - The exposed first node is always head->next. 
  * - Sentinel is tied to the object and not stolen during move operations
- * - Template version not implement for readability purposes
- * 
- * Todo:
- * - Tests
- * 
- */
+ * - Template version not implement for readability purposes */
 
 using Comp = std::function<bool(int, int)>;
 
