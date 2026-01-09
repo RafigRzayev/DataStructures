ae2ec0b691de415285c38de09405bf2e27d3360d
exoplanet1-admin <rafigrzayev7@gmail.com>
2026-01-09 15:55:52 +0400

    duplicate bst for special map implementation

--- Map/main.cpp
+++ Map/main.cpp
@@ -1,12 +1,10 @@
+#include <map>
 #include <iostream>
-#include "BST.hpp"
 
 int main() {
-    BST bst{1, 4, 8, 13, 5, 9, -15, 10, 0, -3, 2};
-    bst.remove(0);
-    bst.remove(10);
-    bst.remove(4);
-    bst.insert(3);
-    bst.preOrder([](int& val) { std::cout << val << std::endl; });
+    std::map<int, double> map;
+    map[1] = 2.5;
+    map[3] = 3;
+    std::cout <<map.count(1) << std::endl; 
     return 0;
 }
--- Map/map.hpp
+++ Map/map.hpp
@@ -1,2 +1,23 @@
 #pragma once
 
+#include "bst.hpp"
+
+template<typename K, typename V>
+class Map {
+    using val_t = std::pair<K, V>;
+public:
+    void insert(const val_t& val);
+    void erase(const K& key);
+private:
+    BST<val_t> bst;
+};
+
+template<typename K, typename V>
+void Map<K,V>::insert(const val_t& val) {
+    bst.insert(val);
+}
+
+template<typename K, typename V>
+void Map<K,V>::erase(const K& key) {
+    bst.remove();
+}
